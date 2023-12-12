b45d6f6f4885ac8803f8028edbb690cfd7881834
ales961 <76012630+ales961@users.noreply.github.com>
2023-12-12 15:05:26 +0300

    Update scr1_tb_log_cmd.sv

--- src/tb/scr1_tb_log_cmd.sv
+++ src/tb/scr1_tb_log_cmd.sv
@@ -10,8 +10,6 @@ always_ff @(posedge scr1_top_tb_ahb.i_top.i_imem_ahb.clk) begin
             // detect command
             
             $display("ADDI, PC: %h", scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.curr_pc);
-            //$display("ADDI, PC2: %h", scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.i_tracelog.trace_pc);
-            //$display("ADDI, PC: %h", scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.i_pipe_exu.pc_curr_ff);
         end
     end
 end
